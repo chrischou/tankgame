9645dc0d20e5ee6a4477561a48c23a357c6b7c51
chrisudn <50350793+chrisudn@users.noreply.github.com>
2023-02-18 18:38:28 +0800

    test:追加單元測試

---
+++ src/test/java/com/trader99/ArrayListSampleTest.java
@@ -0,0 +1,13 @@
+package com.trader99;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListSampleTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
