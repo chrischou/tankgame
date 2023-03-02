919a4aa197838fb9bb1a88f77f1b10a9c1efc10c
chrisudn <50350793+chrisudn@users.noreply.github.com>
2023-03-02 18:11:27 +0800

    feat:追加素材

--- src/main/java/com/trader99/GameClient.java
+++ src/main/java/com/trader99/GameClient.java
@@ -2,28 +2,40 @@ package com.trader99;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
     public GameClient() {
-        this.setPreferredSize(new Dimension(800,600));
+        this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.drawImage(new ImageIcon("asserts/images/cool.png").getImage(),400,100,null);
+        g.drawImage(new ImageIcon("asserts/images/tankD.gif").getImage(), 400, 100, null);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("史上最無聊的坦克大戰");
-        frame.setIconImage(new ImageIcon("asserts/images/cool.png").getImage());
+        frame.setIconImage(new ImageIcon("asserts/images/tank.png").getImage());
         GameClient client = new GameClient();
         client.repaint();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
         frame.setVisible(true);
     }
 }
---
+++ src/main/java/com/trader99/Tank.java
@@ -0,0 +1,10 @@
+package com.trader99;
+
+public class Tank {
+    private int x;
+    private int y;
+
+    private Direction direction;
+
+
+}
